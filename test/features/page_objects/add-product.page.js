92d53d496fb4423e4818eedec80dd1719ae2448f
student <student@safebear.co.uk>
2019-03-06 09:27:39 +0000

    Page objects and step defs added

---
+++ test/features/page_objects/add-product.page.js
@@ -0,0 +1,8 @@
+/**
+* Elements on the page
+*/
+
+var AddProductPage = function() {
+    this.productName = $('#mat-input-0')
+};
+module.exports = new AddProductPage();
---
+++ test/features/page_objects/view-product.page.js
@@ -0,0 +1,9 @@
+/**
+* Page object module for CRUD website
+* @constructor
+*/
+
+var viewProductPage = function() {
+    this.productName = $('#mat-input-0')
+};
+module.exports = new viewProductPage();
